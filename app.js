11459401e4e5f4339ad3de138585e6f2489c15dc
ichoi21 <ihchoi@gmail.com>
2020-06-17 18:22:16 -0700

    revised all js files

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+​const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
 const render = require("./lib/htmlRenderer");
@@ -13,7 +12,84 @@ const render = require("./lib/htmlRenderer");
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+const employeeArray = [];
+let id = 0;
+
+function createEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "type",
+        message: "Please define employee's position to add:",
+        type: "list",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        name: "name",
+        message: "Employee's Name:",
+        type: "input",
+      },
+      {
+        name: "email",
+        message: "Employee's email:",
+        type: "input",
+      },
+      {
+        name: "office",
+        message: "Manager's office number:",
+        type: "input",
+        when: (answers) => answers.type === "Manager",
+      },
+      {
+        name: "github",
+        message: "Enter engineer's github username:",
+        type: "input",
+        when: (answers) => answers.type === "Engineer",
+      },
+      {
+        name: "school",
+        message: "Enter intern's school name:",
+        type: "input",
+        when: (answers) => answers.type === "Intern",
+      },
+    ])
+    .then((res) => {
+      if (res.type === "Manager") {
+        employeeArray.push(new Manager(res.name, id, res.email, res.office));
+        id++;
+      } else if (res.type === "Engineer") {
+        employeeArray.push(new Engineer(res.name, id, res.email, res.github));
+        id++;
+      } else if (res.type === "Intern") {
+        employeeArray.push(new Intern(res.name, id, res.email, res.school));
+        id++;
+      }
+
+      inquirer
+        .prompt({
+          name: "addMore",
+          message: "Done!\nAdd another staff?",
+          type: "confirm",
+        })
+        .then((res) => {
+          if (res.addMore) {
+            createEmployee();
+          } else {
+            const data = render(employeeArray);
+            fs.writeFile("./output/team.html", data, (err) => {
+              if (err) {
+                throw err;
+              } else {
+                console.log("Saved!");
+              }
+            });
+          }
+        });
+    });
+}
+
+createEmployee();​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
